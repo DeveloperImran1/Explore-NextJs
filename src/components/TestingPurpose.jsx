62323193ab65a7cd0989be1e5a84653084a4cf47
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-09-16 13:28:40 +0600

    testing purpose

---
+++ src/components/TestingPurpose.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestingPurpose = () => {
+    return (
+        <div>
+            <h2>New components create for testing purpose</h2>
+        </div>
+    );
+};
+
+export default TestingPurpose;
