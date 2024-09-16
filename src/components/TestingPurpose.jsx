fde2e06547b57c57754a3e60647b58dbc179de61
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-09-16 13:30:20 +0600

    p tag add

--- src/components/TestingPurpose.jsx
+++ src/components/TestingPurpose.jsx
@@ -4,6 +4,7 @@ const TestingPurpose = () => {
     return (
         <div>
             <h2>New components create for testing purpose</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, eius cumque! Quos id natus consequatur, sint facilis non assumenda vero.</p>
         </div>
     );
 };
